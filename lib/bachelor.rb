afa85391f1ac0ecfb31d28c68c73c523ee0b81e8
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-23 22:20:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,7 +8,7 @@ def get_first_name_of_season_winner(data, season)
       data_in_season.each do |data_name|
         data_name.each do |x,y|
           if x == "name" 
-            return y.[0...1]
+            return y[0...1]
           end
       end
     end

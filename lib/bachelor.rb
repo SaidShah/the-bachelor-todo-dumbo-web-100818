3f04fbcf0cdfa1cf9bf5a0b5d5194ce1fe77e7d5
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-24 00:38:25 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,7 +21,11 @@ def get_contestant_name(data, occupation)
  data.each do |season_num, data_in_season|
   data_in_season.each do |data_type|
       data_type.each do |stat_type, value|
-        
+        if stat_type == "occupation"
+          data_type.each do |looking_for, value_needed|
+            
+        end
+      end
     end
   end
  end

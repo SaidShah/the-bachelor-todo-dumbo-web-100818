ab5a6d4ee4306a844df6a1403ee2fe73a901f359
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-24 00:44:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,15 +16,14 @@ def get_first_name_of_season_winner(data, season)
  end
 end
 
+
 def get_contestant_name(data, occupation)
- 
  data.each do |season_num, data_in_season|
   data_in_season.each do |data_type|
       data_type.each do |stat_type, value|
         if stat_type == "occupation" && value == occupation
           data_type.each do |looking_for, value_needed|
             if looking_for == "name"
-             
               return value_needed
           end
         end
@@ -32,11 +31,13 @@ def get_contestant_name(data, occupation)
     end
   end
  end
- 
 end
 
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+ 
+ 
+ 
 end
 
 def get_occupation(data, hometown)
@@ -46,3 +47,39 @@ end
 def get_average_age_for_season(data, season)
   # code here
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
